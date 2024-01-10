acc61b4b62ff6cfa858ffcd9d0dbb2b6595bd555
Samarth Manjunath <30410926+SamarthManjunath@users.noreply.github.com>
2024-01-10 11:31:56 -0600

    Initial commit - App setup

---
+++ src/main/java/com/java/foodSubscription/FoodSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.java.foodSubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSubscriptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/foodSubscription/FoodSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.foodSubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
